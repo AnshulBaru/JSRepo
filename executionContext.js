d79ea387b6ee775a6effd69cea16476f08153d83
AnshulBaru <anshulbaru1141@gmail.com>
2025-02-28 23:20:21 +0530

    new commit

--- executionContext.js
+++ executionContext.js
@@ -103,7 +103,29 @@ console.log(varLocal, letLocal, constLocal);
 
 
 
+var a =10;
+let b = 20;
+const c = 30;
+ 
+function print() {
+    var a= 'ten';
+    let b = 'twenty';
 
+    console.log('****** Inside print Function *******');
+    console.log(a,b,c);  //
+    console.log('******* ******');
+
+    function innerPrint(){
+        var a= 'inner10';
+        console.log('*Inside innerPrint Function*');
+        console.log(a,b,c);
+    }
+    innerPrint();
+
+}
+
+console.log(a,b,c);
+print();
 
 
 
