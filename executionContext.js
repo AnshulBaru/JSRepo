82e8d52b67335e60a5f3f1e760fabcdd30e90eb9
AnshulBaru <anshulbaru1141@gmail.com>
2025-03-04 21:04:43 +0530

    new commit

--- executionContext.js
+++ executionContext.js
@@ -73,64 +73,62 @@
 //GLOBAL
 //FUCNTION(LOCAL)
 //BLOCK
-var varNum = 10;
-let letNum = 20;
-const constNum = 30;
+// var varNum = 10;
+// let letNum = 20;
+// const constNum = 30;
 
-function print() {
-    var varLocal = 50;
-    let letLocal = 60;
-    const constLocal = 70;
-    console.log(varLocal, letLocal, constLocal); // These variable inside the funciton are in fucnitonal/Local scope
-                                                  //They can not be accessed outside the the funciton or block.
-    console.log(varNum, letNum, constNum);
-   
-}
-console.log(varNum, letNum, constNum); //these variables which are in the global scope can be accessed anywhre in the code 
+// function print() {
+//     var varLocal = 50;
+//     let letLocal = 60;
+//     const constLocal = 70;
+//     console.log(varLocal, letLocal, constLocal); // These variable inside the funciton are in fucnitonal/Local scope
+//                                                   //They can not be accessed outside the the funciton or block.
+//     console.log(varNum, letNum, constNum);
 
-print();
-console.log(varLocal, letLocal, constLocal);
+// }
+// console.log(varNum, letNum, constNum); //these variables which are in the global scope can be accessed anywhre in the code 
+
+// print();
+// console.log(varLocal, letLocal, constLocal);
 
 // function numbers(a,b,c){
-    
-    
+
+
 // return a+b+c;
-    
-    
+
+
 // }
 
 // console.log( numbers(1,2,3));
 
 
 
-var a =10;
+var a = 10;
 let b = 20;
 const c = 30;
- 
+
 function print() {
-    var a= 'ten';
-    let b = 'twenty';
+    var a = 'ten';           //Lexical environment is the local memory          
+    let b = 'twenty';        // a and b is stored in this lexical environemt 
+
 
     console.log('****** Inside print Function *******');
-    console.log(a,b,c);  //
+    console.log(a, b, c);  //
     console.log('******* ******');
 
-    function innerPrint(){
-        var a= 'inner10';
+    function innerPrint() {
+        var a = 'inner10';
         console.log('*Inside innerPrint Function*');
-        console.log(a,b,c);
+        console.log(a, b, c);
     }
     innerPrint();
 
 }
 
-console.log(a,b,c);
-print();
+console.log(a, b, c);
+print();   
 
 
 // When a variable is declared in both the global scope and local scope, the variable in the local scope
-// takes precedence and shadows the one in the global scope. 
+// takes precedence and shadows the one in the global scope.
 
-//JavaScript lexical environment is a data structure with an identifier-variable mapping. Where identifier
-//  refers to the name of variables/functions and variable is the reference to the actual object. It also
-//  holds a reference to a parent lexical environment.
