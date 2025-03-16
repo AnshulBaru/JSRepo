a7180fe1be97f471ca024cf159d67b5239ff2211
AnshulBaru <anshulbaru1141@gmail.com>
2025-03-16 15:58:53 +0530

    new commit

--- Functions.js
+++ Functions.js
@@ -6,3 +6,12 @@ function sum(a, b) {
 
 const sum1 = sum(5, 6);
 console.log(sum1);
+
+//Function expression:
+//Function expression means assigning a fucntion to a variable 
+
+const sum2= function (x,y){              //no need to give name to a function here in function expression
+return x+y;                      //so such functions which doesn't have name are called as anonymous functions
+
+} 
+console.log(sum2(1,8));
