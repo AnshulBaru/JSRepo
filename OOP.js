99273efb33e097c2e8b3f92bbb8c5b4155e95446
AnshulBaru <anshulbaru1141@gmail.com>
2025-05-27 23:18:42 +0530

    new commit

--- OOP.js
+++ OOP.js
@@ -1,60 +1,69 @@
 
 
-const movie = {
-    title: "The Avengers",
-    year: 2012,
-    genre: "Action, Sci-Fi, Thriller",
-    cast: {
-        main_lead: "Robert Downey Jr.",
-        others: "Chris Evans",
-    },
-    getDetails() {
-        //     console.log(`
-        //          Title: ${movie.title}
-        //          Genre: ${movie.year}
-        //          Cast: ${movie.cast} `) 
-        // },    // this form of object is called object literals
-    //     console.log(`
-    //     Title: ${movie.title}
-    //     Genre: ${movie.year}
-    //     Cast: ${movie.cast} `)
-    // },
+// const movie = {
+//     title: "The Avengers",
+//     year: 2012,
+//     genre: "Action, Sci-Fi, Thriller",
+//     cast: {
+//         main_lead: "Robert Downey Jr.",
+//         others: "Chris Evans",
+//     },
+//     getDetails() {
+//         //     console.log(`
+//         //          Title: ${movie.title}
+//         //          Genre: ${movie.year}
+//         //          Cast: ${movie.cast} `) 
+//         // },    // this form of object is called object literals
+//     //     console.log(`
+//     //     Title: ${movie.title}
+//     //     Genre: ${movie.year}
+//     //     Cast: ${movie.cast} `)
+//     // },
 
-     console.log(`
-        Title: ${this.title}
-        Genre: ${this.year}
-        Cast: ${this.cast} `)        // we can also replace movie by 'this' keyword b
-    },
-    
-//if we write this is place of movie then we'll get the same output.
-// this allows us to access and manipulte object properties and methods within the scope of an object
+//      console.log(`
+//         Title: ${this.title}
+//         Genre: ${this.year}
+//         Cast: ${this.cast} `)        // we can also replace movie by 'this' keyword b
+//     },
 
-
-
-    getMovieDetails(detail) {
-        console.log(movie[detail])   // by using bracket notation  
-    }
+// //if we write this is place of movie then we'll get the same output.
+// // this allows us to access and manipulte object properties and methods within the scope of an object
 
 
 
-};
-console.log(movie.title);
+//     getMovieDetails(detail) {
+//         console.log(movie[detail])   // by using bracket notation  
+//     }
 
-movie.getDetails();
 
-const detail = "year";
-movie.getMovieDetails(detail);
+
+// };
+// console.log(movie.title);
+
+// movie.getDetails();
+
+// const detail = "year";
+// movie.getMovieDetails(detail);
 
 
 //Construction function:
 
-const movieObj={
-    title: 'The Avengers',
-    year: 2012,
-    getDetails(){
-        console.log(`
+
+
+function movie(title, year) {
+
+    const movieObj = {
+        title: title,
+        year: year,
+        getDetails() {
+            console.log(`
             Title:${this.title}
-            Year: ${this.year}`);
-    
+            Year: ${this.year}
+             `);
+
         },
-};
+    };
+
+
+    
+}
