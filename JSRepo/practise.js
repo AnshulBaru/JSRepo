27ae104b8b546a92a8d01fe71cb830333a1ffd0d
AnshulBaru <anshulbaru1141@gmail.com>
2026-02-14 11:24:28 +0530

    new commit

--- JSRepo/practise.js
+++ JSRepo/practise.js
@@ -29,36 +29,37 @@
 
 // console.log(students.School.SchoolName);
 
-function Vehicle(name, color, wheels) {
-  this.name = name;
-  this.color = color;
-  this.wheels = wheels;
-}
-Vehicle.prototype.getDetails = function () {
-  console.log(`
-    The ${this.name} is ${this.color} in color.
-      It has ${this.wheels} wheels`);
-};
+// function Vehicle(name, color, wheels) {
+//   this.name = name;
+//   this.color = color;
+//   this.wheels = wheels;
+// }
+// Vehicle.prototype.getDetails = function () {
+//   console.log(`
+//     The ${this.name} is ${this.color} in color.
+//       It has ${this.wheels} wheels`);
+// };
 
-const car = new Vehicle("Car", "blue", 4);
+// const car = new Vehicle("Car", "blue", 4);
 
 // console.log(car.hasOwnProperty("getDeatils"));
 
-car.getDetails();
+// car.getDetails();
 
-class Cars {
-  constructor(model, colour) {
-    this.model = model;
-    this.colour = colour;
-  }
-  name() {
-    console.log(`This is a ${this.model} which is ${this.colour} in color`);
-  }
-}
-let c1 = new Cars("Porshe GTR", "Red");
-let c2 = new Cars("Toyota Tacoma", "White");
+// class Cars {
+//   constructor(model, colour) {
+//     this.model = model;
+//     this.colour = colour;
+//   }
+//   name() {
+//     console.log(`This is a ${this.model} which is ${this.colour} in color`);
+//   }
+// }
+// let c1 = new Cars("Porshe GTR", "Red");
+// let c2 = new Cars("Toyota Tacoma", "White");
 
-c1.name();
-c2.name();
+// c1.name();
+// c2.name();
 
-console.log({} + []);
+// console.log({} + []);
+
